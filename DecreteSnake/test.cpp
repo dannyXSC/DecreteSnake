9e2f5db921c0901278c4ec56d73e4d12d13e160f
danny <874794016@qq.com>
2020-12-07 12:48:20 +0800

    Auto stash before merge of "main" and "DecreteSnake/first"

--- DecreteSnake/test.cpp
+++ DecreteSnake/test.cpp
@@ -5,10 +5,17 @@ using namespace std;
 int main()
 {
     srand(time(0));
+<<<<<<< Updated upstream
 
     while(1)
     {
         cout << rand() << endl;
         Sleep(100);
+=======
+    while(1)
+    {
+        cout<<rand()<<endl;
+        usleep(100*1000);
+>>>>>>> Stashed changes
     }
 }
